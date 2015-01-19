36726e8861ac026d035181f1c15a6923f2224eb3
joromir <h8guitar@gmail.com>
2015-01-19 21:15:28 +0200

    class DB

---
+++ DB.rb
@@ -0,0 +1,31 @@
+module RBHangman
+  class DB
+    def has_record?(table, value, column)
+      query = "SELECT #{column.upcase}
+               FROM #{table.upcase}
+               WHERE #{column.upcase}='#{value.upcase}';"
+      SQLite3::Database.new("./rbhangman.db").execute(query) != []
+    end
+
+    def selector(select, from, where = nil, order_by = nil)
+      select = "SELECT " + select
+      from = " FROM " + from
+      where = where ? " WHERE " + where : ""
+      order_by = order_by ? " ORDER BY " + order_by : ""
+
+      SQLite3::Database.new("./rbhangman.db")
+                       .execute(select + from + where + order_by)
+    end
+
+    def inserter(table, values, columns)
+      query = "INSERT INTO #{table} (#{columns})
+               VALUES (#{values})"
+      SQLite3::Database.new("./rbhangman.db").execute(query)
+    end
+
+    def updater(update, set, where)
+      SQLite3::Database.new("./rbhangman.db")
+                       .execute("UPDATE #{update} SET #{set} WHERE #{where};")
+    end
+  end
+end
